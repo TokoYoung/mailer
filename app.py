e6afa2b247cdee03133a058a6d07ae776e3e1983
Tornike Machavariani <tornike.machavariani@singular.uk>
2021-02-11 22:41:19 +0400

    first test commit

---
+++ app.py
@@ -0,0 +1,95 @@
+from flask import Flask,render_template,url_for,request,request
+from psql_functions import *
+
+
+torus = Flask(__name__)
+
+
+@torus.route("/",methods=['GET', 'POST'] )
+def torusa():
+    return  render_template('index.html')
+
+
+@torus.route('/add_contacts', methods=["POST", "GET"])
+def add_contacts():
+    if request.method == "GET":
+        return render_template("add_contacts.html")
+    else:
+        name = request.form["full_name"]
+        companyid = request.form["companyid"]
+
+        insertion = {
+            "full_name" : name,
+            "companyid" : companyid
+        }
+
+        Insert(insertion)
+
+        return redirect("/")
+
+
+@torus.route("/all_contacts",methods=['GET', 'POST'] )
+def function():
+    if request.method == "GET":
+        contacts = []
+        _all = []            
+        rows = Select_All()
+        for row in rows:
+            each = {
+                "_id" : row[0],
+                "full_name" : row[1],
+                "companyid" : row[2]
+            }
+            _all.append(each)
+        for each in _all:
+            if each["full_name"] != "":
+                insertion = {
+                    "id" : str(each["_id"]),
+                    "full_name" : each["full_name"],
+                    "companyid" : each["companyid"]
+                }
+                contacts.append(insertion)
+        return render_template("all_contacts.html", contacts=contacts)
+
+@torus.route('/contacts_modification', methods=["POST", 'GET'])
+def contacts_modification():
+    if request.method == "GET":
+        _id = request.args.get("id")
+        name = request.args.get("full_name")
+        comp = request.args.get("company")
+        
+        # GET DATA FROM PSQL
+        row = Select_One(_id)
+        data = {
+            "id" : row[0],
+            "full_name" : row[1],
+            "companyid" : row[2]
+        }
+
+        print(data)
+        return render_template('contacts_modification.html', data=data)
+    else:
+        fullname = request.form["full_name"]
+        _id = request.args.get("id")
+
+        Update(_id, full_name)
+
+        return redirect("/")
+
+@torus.route('/delete_contacts', methods=["POST", 'GET'])
+def delete_contact():
+    if request.method == "GET":
+        _id = request.args.get("id")
+        _route = request.args.get("route")
+        Delete_One(_id)
+        
+    #     if _route == "answered":
+    #         return redirect("/messages_answered")
+    #     else:    
+    #         return redirect("/messages_unanswered")
+    # else:
+    #     return "It does not take post request"
+
+
+if __name__ == '__main__':
+    torus.run(host="0.0.0.0", port=8080, debug=True)
---
+++ psql_functions.py
@@ -0,0 +1,158 @@
+import psycopg2
+import sys
+import os
+
+# conf = {
+#       "host" : os.environ['DB_HOST'],
+#       "database" : os.environ['DB_NAME'],
+#       "user" : os.environ['DB_USER'],
+#       "password" : os.environ['DB_PASS'],
+#       "port" : os.environ['DB_PORT']
+# }
+
+conf = {
+    "database" : "mailer",
+    "user" : "postgres",
+    "password" : "Pass2020!",
+    "host" : "172.16.38.15", 
+    "port" : "5432"
+}
+
+
+# Conversation Table
+def Insert(data):
+    conn = psycopg2.connect(database=conf["database"], user = conf["user"], password = conf["password"], host = conf["host"], port = conf["port"])
+    cur = conn.cursor()
+
+    fullname = data['fullname']
+    companyid = data['companyid']
+    
+    cur.execute("INSERT INTO main_users (full_name, companyid) VALUES (%s, %s);", (fullname, companyid))
+
+    conn.commit()
+    print("Records created successfully")
+    conn.close()
+
+
+def Select_All():
+    conn = psycopg2.connect(database=conf["database"], user = conf["user"], password = conf["password"], host = conf["host"], port = conf["port"])
+    cur = conn.cursor()
+
+    cur.execute("SELECT ID, full_name, companyid from main_users")
+    rows = cur.fetchall()
+    # for row in rows:
+    #     print(row[0], row[1], row[2])
+    print("Operation done successfully")
+    conn.close()
+
+    return rows
+
+
+def Select_One(_id):
+    conn = psycopg2.connect(database=conf["database"], user = conf["user"], password = conf["password"], host = conf["host"], port = conf["port"])
+    cur = conn.cursor()
+
+    cur.execute("SELECT ID, full_name, companyid from main_users WHERE ID = %s;", [_id])
+    row = cur.fetchone()
+    # print(row)
+    print("Operation done successfully")
+    conn.close()
+
+    return row
+
+
+def Select_One_By_Statement(statement):
+    conn = psycopg2.connect(database=conf["database"], user = conf["user"], password = conf["password"], host = conf["host"], port = conf["port"])
+    cur = conn.cursor()
+
+    cur.execute("SELECT * from conversation WHERE statement = %s;", [statement])
+    row = cur.fetchone()
+    # print(row)
+    print("Operation done successfully")
+    conn.close()
+
+    return row
+
+
+def Update(_id, response):
+    conn = psycopg2.connect(database=conf["database"], user = conf["user"], password = conf["password"], host = conf["host"], port = conf["port"])
+    cur = conn.cursor()
+
+    cur.execute("UPDATE conversation set response = %s where ID = %s;", (response, _id))
+    conn.commit()
+    print("Total number of rows updated: ", cur.rowcount)
+    conn.close()
+
+
+def Delete_One(_id):
+    conn = psycopg2.connect(database=conf["database"], user = conf["user"], password = conf["password"], host = conf["host"], port = conf["port"])
+    cur = conn.cursor()
+
+    cur.execute("DELETE from conversation where ID = %s;", [_id])
+    conn.commit()
+    print("Total number of rows deleted :", cur.rowcount)
+    conn.close()
+
+
+def Delete_All():
+    conn = psycopg2.connect(database=conf["database"], user = conf["user"], password = conf["password"], host = conf["host"], port = conf["port"])
+    cur = conn.cursor()
+
+    cur.execute("DELETE from conversation where ID > 0;")
+    conn.commit()
+    print("Total number of rows deleted :", cur.rowcount)
+    conn.close()
+
+
+
+
+
+
+# Users Table
+def Select_All_Users():
+    conn = psycopg2.connect(database=conf["database"], user = conf["user"], password = conf["password"], host = conf["host"], port = conf["port"])
+    cur = conn.cursor()
+
+    cur.execute("SELECT * from fb_users")
+    rows = cur.fetchall()
+    # for row in rows:
+    #     print(row[0], row[1], row[2])
+    print("Operation done successfully")
+    conn.close()
+
+    return rows
+
+
+def Select_One_User(fb_id):
+    conn = psycopg2.connect(database=conf["database"], user = conf["user"], password = conf["password"], host = conf["host"], port = conf["port"])
+    cur = conn.cursor()
+
+    cur.execute("SELECT * from fb_users WHERE fb_id = %s;", [fb_id])
+    row = cur.fetchone()
+    # print(row)
+    print("Operation done successfully")
+    conn.close()
+
+    return row
+
+
+def Insert_User(data):
+    conn = psycopg2.connect(database=conf["database"], user = conf["user"], password = conf["password"], host = conf["host"], port = conf["port"])
+    cur = conn.cursor()
+
+    first_name = data['first_name']
+    last_name = data['last_name']
+    profile_pic = data['profile_pic']
+    fb_id = data['id']
+
+    cur.execute("INSERT INTO fb_users (first_name, last_name, profile_pic, fb_id) VALUES (%s, %s, %s, %s);", (first_name, last_name, profile_pic, fb_id))
+
+    conn.commit()
+    print("Records created successfully")
+    conn.close()
+
+
+
+
+
+
